54d78c5c37d91db27483e1d84be4cc5fc29b9fb7
he2joojo <121915790+he2joojo@users.noreply.github.com>
2023-04-14 21:46:33 +0900

    Delete src/test/java/kr/codesqaud/cafe/controller directory

--- src/test/java/kr/codesqaud/cafe/controller/ArticleControllerTest.java
+++
@@ -1,8 +0,0 @@
-package kr.codesqaud.cafe.controller;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(ArticleController.class)
-class ArticleControllerTest {
-
-}
--- src/test/java/kr/codesqaud/cafe/controller/UserControllerTest.java
+++
@@ -1,9 +0,0 @@
-package kr.codesqaud.cafe.controller;
-
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(UserController.class)
-class UserControllerTest {
-
-}
